3830ca0a7b9fad10118bab8c9afefa4f2e3ffe48
Jin Hao <limjinhao@gmail.com>
2021-11-20 15:39:00 +0800

    Time: 0 ms (100.00%), Space: 39.3 MB (47.72%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int single = 0;
-        for(int i = 0; i < nums.length; i++) {
-            single = single ^ nums[i];
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mid % 2 == 1) mid--;
+
+            if (nums[mid] == nums[mid + 1]) {
+                // to the right
+                start = mid + 2;
+            } else {
+                // to the left
+                end = mid;
+            }
         }
-        return single;
+        
+        return nums[start];
     }
 }
