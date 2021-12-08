3b8f7958eac33e0358021eaf4e6b99e55d297005
Jin Hao <limjinhao@gmail.com>
2021-12-08 12:48:00 +0800

    Time: 0 ms (100.00%), Space: 39.1 MB (60.95%) - LeetHub

---
+++ binary-tree-tilt/binary-tree-tilt.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public int findTilt(TreeNode root) {
+        getSum(root);
+        return sum;
+    }
+    
+    public int getSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = getSum(root.left);
+        int right = getSum(root.right);
+        
+        sum += Math.abs(left - right);
+        
+        return right + left + root.val;
+    }
+}
