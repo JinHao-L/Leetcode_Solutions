f13cad9ae8e545aa0f791cf8c0d6c94a6b8bed72
Jin Hao <limjinhao@gmail.com>
2022-08-15 22:33:20 +0800

    Time: 11 ms (45.05%), Space: 46.6 MB (37.45%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,19 +1,28 @@
 class Solution {
-    static final String[] SYM = {"IV", "IX", "XL", "XC", "CD", "CM", "M", "D", "C", "L", "X", "V", "I"};
-    static final int[] VAL = {4, 9, 40, 90, 400, 900, 1000, 500, 100, 50, 10, 5, 1};
+    Map<Character, Integer> map = new HashMap<>();
+    public Solution() {
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+    }
     
     public int romanToInt(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
-        
-        
-        for(int i = 0; i < SYM.length; i++) {
-            if (s.startsWith(SYM[i])) {
-                return VAL[i] + romanToInt(s.substring(SYM[i].length()));
+        int sum = 0;
+        char[] arr = s.toCharArray();
+        for(int i = 0; i < arr.length; i++) {
+            int val = map.get(arr[i]);
+            
+            if (i < arr.length - 1 && val < map.get(arr[i + 1])) {
+                sum -= val;
+            } else {
+                sum += val;
             }
         }
         
-        return 0;
+        return sum;
     }
 }
